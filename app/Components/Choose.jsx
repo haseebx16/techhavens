7cb36b648d63344065a81de5df73d67ceabbcbcd
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-01-22 00:18:43 +0500

    theme changes

--- app/Components/Choose.jsx
+++ app/Components/Choose.jsx
@@ -26,7 +26,7 @@ const Choose = () => {
         </section>
         <section className='md:w-[50%] z-20 pt-12 md:pt-0 w-full'>
             <div>
-                <p className='md:text-3xl text-2xl uppercase text-cyan-500'>Curious Why-</p>
+                <p className='md:text-3xl text-2xl uppercase text-cards'>Curious Why-</p>
                 <h1 className='md:text-5xl text-3xl font-bold text-black mt-6'>People Choose Tech Haven</h1>
                 <p className='text-md text-gray-600 mt-6'>We are a platform that offers amazing career opportunities to everyone who shakes our hand.</p>
                 <p className='text-md text-gray-600 mt-6'>&#x2022;&nbsp; Vetted once, eligible for life</p>
--- app/Components/Identity.jsx
+++ app/Components/Identity.jsx
@@ -26,24 +26,24 @@ const Identity = () => {
         </section>
         <section className='md:w-[50%] pt-20 md:pt-0 w-full z-20'>
             <div>
-                <p className='md:text-3xl text-2xl uppercase text-cyan-500'>Tech Haven PVT LTD--</p>
+                <p className='md:text-3xl text-2xl uppercase text-cards'>Tech Haven PVT LTD--</p>
                 <h1 className='md:text-5xl text-3xl font-bold text-black mt-6'>To Be The Blueprint for Your Digital Identity.</h1>
                 <p className='text-md text-gray-600 mt-6'>At Tech Haven, build the perfect team for your project whenever needed! This enables you to save time, focus on your project, and ensure successful completion.</p>
                 <div className='grid md:grid-cols-2 mt-6 gap-4 '>
                     <div className='p-4  '>
-                        <h1 className='text-2xl text-cyan-500 '>Speedy Hiring Process</h1>
+                        <h1 className='text-2xl text-cards '>Speedy Hiring Process</h1>
                         <p className='text-black text-justify pt-4'>Our mission is to provide the best possible service to our clients and to help them achieve their goals.</p>
                     </div>
                     <div className='p-4  duration-300'>
-                        <h1 className='text-2xl text-cyan-500 text-justify'> ONLY THE BEST OF THE BEST</h1>
+                        <h1 className='text-2xl text-cards text-justify'> ONLY THE BEST OF THE BEST</h1>
                         <p className='text-black text-justify pt-4'>Our vision is to be the best in the industry and to provide the best possible service to our clients.</p>
                     </div>
                     <div className='p-4  duration-300'>
-                        <h1 className='text-2xl text-cyan-500 text-justify'>FUTURE-PROOFED TALENT                        </h1>
+                        <h1 className='text-2xl text-cards text-justify'>FUTURE-PROOFED TALENT                        </h1>
                         <p className='text-black text-justify pt-4'>Our mission is to provide the best possible service to our clients and to help them achieve their goals.</p>
                     </div>
                     <div className='p-4  hover:text-white duration-300'>
-                        <h1 className='text-2xl text-cyan-500 text-justify'>EXCEPTIONAL PROFESSIONALISM                        </h1>
+                        <h1 className='text-2xl text-cards text-justify'>EXCEPTIONAL PROFESSIONALISM                        </h1>
                         <p className='text-black text-justify pt-4'>Our vision is to be the best in the industry and to provide the best possible service to our clients.</p>
                     </div>
                 </div>
